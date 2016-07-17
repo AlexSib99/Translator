a2f0d05f8502d5b3a8fe8e394f7ff1823212e7ed
AlexSib99 <luckyanov@gmail.com>
2016-07-18 04:10:07 +0700

    fix sintax and optimise logic

--- app.js
+++ app.js
@@ -9,18 +9,12 @@ function translateNumber(inputNumber) {
 
     for (i = number.length, k = 0; i > 0; i = i -3, k++) {
         n = i - 3;
-
-        if (n < 0) {
-            n = 0;
-        }
-
+        n = n < 0 ? 0 : n;
         triplets[k] = number.slice(n, i);
     }
-    for (j = triplets.length - 1; j >= 0; j--) {
 
-        if (triplets[j] == 0) {
-            continue;
-        }
+    for (j = triplets.length - 1; j >= 0; j--) {
+        if (triplets[j] == 0) continue;
 
         if (translation === '') {
             translation += translateLargeNumbers(j, triplets[j]);
@@ -32,44 +26,36 @@ function translateNumber(inputNumber) {
     return translation;
 }
 
-function translateThreeDigits(threeDigits) {
-    var twoDigits,
-        thirdDigit,
-        words = ['один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять',
-        'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать',
-        'девятнадцать', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто',
-        'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'];
+function translateThreeDigits(digits) {
+    var words = [
+            ['', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять', 'одиннадцать',
+            'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать',
+            'девятнадцать'],
+            ['', '', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто'],
+            ['', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот']
+        ],
+        result = '';
 
-    function translateTwoDigits(digits) {
-        var firstDigit,
-            secondDigit;
+        if (digits.length == 3) {
+            result += words[2][digits[0]];
+            digits = digits.slice(1, 3);
 
-        if (digits < 21) {
-            return words[digits - 1];
-        } else {
-            firstDigit = digits % 10;
-            secondDigit = Math.floor(digits / 10);
-
-            if (firstDigit == 0) {
-                return words[secondDigit + 17];
-            } else {
-                return words[secondDigit + 17] + ' ' + words[firstDigit - 1];
+            if (digits != 0 && result !== '') {
+                result += ' ';
             }
         }
-    }
 
-    twoDigits = threeDigits % 100;
-    thirdDigit = Math.floor(threeDigits / 100);
-
-    if (thirdDigit == 0) {
-        return translateTwoDigits(threeDigits);
-    } else {
-        if (twoDigits == 0) {
-            return words[thirdDigit + 26];
+        if (digits < 20) {
+            result += words[0][Number(digits)];
         } else {
-            return words[thirdDigit + 26] + ' ' + translateTwoDigits(twoDigits);
+            result += words[1][digits[0]];
+
+            if (digits[1] != 0) {
+                result += ' ' + words[0][digits[1]];
+            }
         }
-    }
+
+        return result;
 }
 
 function translateLargeNumbers(tripletsNumber, triplet) {
@@ -82,17 +68,22 @@ function translateLargeNumbers(tripletsNumber, triplet) {
         tripletInWords = translateThreeDigits(triplet);
 
     switch(tripletsNumber) {
-        case 0: return tripletInWords;
+        case 0:
+            return tripletInWords;
         case 1:
             if (Math.floor(triplet / 10) % 10 == 1) {
                 return tripletInWords + ' ' + largeNumbers[tripletsNumber];
             } else {
                 switch(triplet % 10) {
-                    case 1: return tripletInWords.slice(0, tripletInWords.length - 2) + 'на' + ' ' + largeNumbers[tripletsNumber] + 'а';
-                    case 2: return tripletInWords.slice(0, tripletInWords.length - 1) + 'е' + ' ' + largeNumbers[tripletsNumber] + 'и';
-                    case 3: return tripletInWords + ' ' + largeNumbers[tripletsNumber] + 'и';
-                    case 4: return tripletInWords + ' ' + largeNumbers[tripletsNumber] + 'и';
-                    default: return tripletInWords + ' ' + largeNumbers[tripletsNumber];
+                    case 1:
+                        return tripletInWords.slice(0, tripletInWords.length - 2) + 'на' + ' ' + largeNumbers[tripletsNumber] + 'а';
+                    case 2:
+                        return tripletInWords.slice(0, tripletInWords.length - 1) + 'е' + ' ' + largeNumbers[tripletsNumber] + 'и';
+                    case 3:
+                    case 4:
+                        return tripletInWords + ' ' + largeNumbers[tripletsNumber] + 'и';
+                    default:
+                        return tripletInWords + ' ' + largeNumbers[tripletsNumber];
                 }
             }
         default:
@@ -100,11 +91,14 @@ function translateLargeNumbers(tripletsNumber, triplet) {
                 return tripletInWords + ' ' + largeNumbers[tripletsNumber] + 'ов';
             } else {
                 switch(triplet % 10) {
-                    case 1: return tripletInWords + ' ' + largeNumbers[tripletsNumber];
-                    case 2: return tripletInWords + ' ' + largeNumbers[tripletsNumber] + 'а';
-                    case 3: return tripletInWords + ' ' + largeNumbers[tripletsNumber] + 'а';
-                    case 4: return tripletInWords + ' ' + largeNumbers[tripletsNumber] + 'а';
-                    default: return tripletInWords + ' ' + largeNumbers[tripletsNumber] + 'ов';
+                    case 1:
+                        return tripletInWords + ' ' + largeNumbers[tripletsNumber];
+                    case 2:
+                    case 3:
+                    case 4:
+                        return tripletInWords + ' ' + largeNumbers[tripletsNumber] + 'а';
+                    default:
+                        return tripletInWords + ' ' + largeNumbers[tripletsNumber] + 'ов';
                 }
             }
     }
